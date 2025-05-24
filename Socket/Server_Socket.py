17adf821db3d7ea9900ab9a786e399477b01370b
Antonynixenraj <antonynixen@gmail.com>
2025-05-24 10:45:52 +0530

    One to one chat server completed

--- Socket/Server_Socket.py
+++ Socket/Server_Socket.py
@@ -1,38 +1,17 @@
-# first of all import the socket library
 import socket
 
-# next create a socket object
-s = socket.socket()
-print("Socket successfully created")
-
-# reserve a port on your computer in our
-# case it is 12345 but it can be anything
-port = 12345
-
-# Next bind to the port
-# we have not typed any ip in the ip field
-# instead we have inputted an empty string
-# this makes the server listen to requests
-# coming from other computers on the network
-s.bind(('', port))
-print("socket binded to %s" % (port))
-
-# put the socket into listening mode
+s=socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Use to reuse the same port for quick reuse of server
+s.bind(('',1234))
 s.listen(5)
-print("socket is listening")
-
-# a forever loop until we interrupt it or
-# an error occurs
-while True:
-    # Establish connection with client.
-    c, addr = s.accept()
-    print('Got connection from', addr)
-
-    # send a thank you message to the client. encoding to send byte type.
-    c.send('Thank you for connecting'.encode())
-
-    # Close the connection with the client
-    c.close()
-
-    # Breaking once connection closed
-    break
+print('Socket is listening.....')
+c,add=s.accept()
+print('Got connection from ',add)
+while(True):
+    n=input('>> ')
+    c.send(n.encode())
+    msg=c.recv(1024).decode()
+    if(msg=="exit" or n=="exit"):
+        break
+    print('$ '+msg)
+c.close()
